22dda8aee32e2461ee38e0ecaf1311112c9def36
JoseGómezHidalgo <pepegomezman@gmail.com>
2024-04-09 14:03:18 +0200

    "Tercer commit : Proyecto inicial en rama master - Jose_Gómez_Hidalgo."

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println(" Rama principal master trunk");
+        // Rama BranchFuncionalidad1
+        System.out.println(" Segunda modificación en la rama dentro del método main, en la rama BranchFuncionalidad1");
     }
 }
